b8cc3858427e45a41f4add48a08e8870f4ce8729
Bogdankmennyy <bogdankmennyj@gmail.com>
2023-09-12 20:18:53 +0300

    practic

--- Parallel programming of the library/MainWindow.xaml.cs
+++ Parallel programming of the library/MainWindow.xaml.cs
@@ -1,28 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace Parallel_programming_of_the_library
+namespace WpfTaskExample
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Спосіб 1: Використовуємо метод Start класу Task
+            Task task1 = new Task(DisplayCurrentTime);
+            task1.Start();
+
+            // Спосіб 2: Використовуємо метод Task.Factory.StartNew
+            Task task2 = Task.Factory.StartNew(DisplayCurrentTime);
+
+            // Спосіб 3: Використовуємо метод Task.Run
+            Task task3 = Task.Run(DisplayCurrentTime);
+
+            // Очікуємо завершення всіх завдань
+            Task.WhenAll(task1, task2, task3).ContinueWith((t) =>
+            {
+                // Після завершення виводимо повідомлення
+                MessageBox.Show("Всі завдання завершено.");
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+        }
+
+        private void DisplayCurrentTime()
+        {
+            DateTime currentTime = DateTime.Now;
+
+            // Оновлюємо інтерфейс з потоку користувальницького інтерфейсу
+            Dispatcher.Invoke(() =>
+            {
+                TimeLabel.Text = $"Поточний час і дата: {currentTime}";
+            });
         }
     }
 }
